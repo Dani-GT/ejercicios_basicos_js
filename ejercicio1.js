e563362afc1bff808d14dc9bec94e097163c8b6e
Dani-GT <dani_spain87@hotmail.com>
2024-07-14 16:28:41 +0200

    Create ejercicio1.js
    ejercicio completado

---
+++ ejercicio1.js
@@ -0,0 +1,24 @@
+let miSuperheroeFavorito;
+miSuperheroeFavorito = "Hulk"
+
+console.log(miSuperheroeFavorito)
+
+let NumeroFavorito;
+NumeroFavorito = 50;
+
+console.log(NumeroFavorito)
+
+let altura;
+altura = 5;
+
+console.log(altura)
+
+let longitud;
+longitud = 10;
+
+console.log(longitud)
+
+let suma = (altura + longitud)
+
+console.log(suma)
+
