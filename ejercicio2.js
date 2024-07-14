27c6dd6f37ad1fdc415c522c47ff2e1c917eaa55
Dani-GT <dani_spain87@hotmail.com>
2024-07-14 17:34:27 +0200

    2.4 hecho

--- ejercicio2.js
+++ ejercicio2.js
@@ -29,6 +29,19 @@ console.log(total)
 //2.4
 
 
+let precioBaseGlobal = 10000;
+
+precioBaseGlobal = 25000;
+
+const nave1 = {nombre: "Ala-X", precioBase: 50000, precioFinal: 60000};
+const nave2 = {nombre: "Halcón Milenario", precioBase: 70000, precioFinal: 80000};
+
+
+nave1.precioFinal = (precioBaseGlobal + nave1.precioBase)
+nave2.precioFinal = (precioBaseGlobal + nave2.precioBase)
+
+console.log(nave1.precioFinal)
+console.log(nave2.precioFinal)
 
 
 
