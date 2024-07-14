228aa267d07fe8e72ffaf59aa8068dbc73fc871d
Dani-GT <dani_spain87@hotmail.com>
2024-07-14 17:23:15 +0200

    ejercicio2.3 hecho

--- ejercicio2.js
+++ ejercicio2.js
@@ -6,6 +6,7 @@ jedi.edad = 25;
 
 console.log (jedi)
 
+
 //2.2
 
  let nombre = "Leia";
@@ -14,9 +15,20 @@ console.log (jedi)
 
  console.log("Soy "+ nombre + apellido  + ",tengo "+ edad + "años y soy una princesa de Alderaan")
 
+
  //2.3
 
- 
+const sable1 = {nombre: "Shoto de Yoda", precio: 1500};
+const sable2 = {nombre: "Sable de Darth Vader", precio: 2000};
+
+total = (sable1.precio + sable2.precio)
+
+console.log(total)
+
+
+//2.4
+
+
 
 
 
