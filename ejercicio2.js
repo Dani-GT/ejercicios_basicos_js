47b87a8b33be8d63b6c03d376a3851959bd3183c
Dani-GT <dani_spain87@hotmail.com>
2024-07-14 17:20:54 +0200

    ejercicio 2 completado

---
+++ ejercicio2.js
@@ -0,0 +1,22 @@
+//2.1
+
+const jedi = {nombre: "Luke Skywalker", edad: 19};
+
+jedi.edad = 25;
+
+console.log (jedi)
+
+//2.2
+
+ let nombre = "Leia";
+ let apellido = "Organa";
+ let edad = 20;  
+
+ console.log("Soy "+ nombre + apellido  + ",tengo "+ edad + "años y soy una princesa de Alderaan")
+
+ //2.3
+
+ 
+
+
+
