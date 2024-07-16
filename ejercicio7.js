c4e0ac771b13c5c2e6dfacde3d4b4319c6cbc799
Dani-GT <dani_spain87@hotmail.com>
2024-07-16 22:13:15 +0200

    Update ejercicio7.js

--- ejercicio7.js
+++ ejercicio7.js
@@ -1,7 +1,8 @@
+let numberOne = 4;
+let numberTwo = 7;
+
 function greaterNumber(numberOne , numberTwo) {
-    let numberOne = 4;
-    let numberTwo = 7;
     
    }
  
-   console.log(Math.max(numberOne, numberTwo));
+   console.log(Math.max(numberOne, numberTwo)); 
