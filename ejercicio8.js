9188891b9065bb278c880e2d282c5c4743fe8ecf
Dani-GT <dani_spain87@hotmail.com>
2024-07-17 00:29:24 +0200

    finish 8

--- ejercicio8.js
+++ ejercicio8.js
@@ -1,6 +1,12 @@
 const avengers = ['Hulk', 'Thor', 'Iron Man', 'Captain A.', 'Spiderman', 'Captain M.'];
 function findLongestWord(stringList) {
-  // Completar código
+  let longestWord = "";
+  for (const word of stringList) {
+    if (word.length > longestWord.length){
+      longestWord = word;
+    }
+  }
+console.log(longestWord);
 }
 
-
+findLongestWord(avengers)
