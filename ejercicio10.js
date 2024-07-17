1817b5091d160918522f51ee6ef2703479a317a2
Dani-GT <dani_spain87@hotmail.com>
2024-07-17 19:36:36 +0200

    finish ejer 10

--- ejercicio10.js
+++ ejercicio10.js
@@ -1,4 +1,11 @@
 const numbers = [12, 21, 38, 5, 45, 37, 6];
+
 function average(numberList) {
-  // Completar
+ let suma = 0;
+  for(let i = 0; i < numberList.length; i++){
+    suma += numberList[i];
+  } 
+  return suma/numberList.length
 }
+average(numbers)
+console.log(average(numbers))
