3535cd18138e423880b195b0e71af95a654aef91
Dani-GT <dani_spain87@hotmail.com>
2024-08-22 00:24:02 +0200

    add text

--- ejercicio19.js
+++ ejercicio19.js
@@ -1,3 +1,7 @@
+// Mixed For...of e includes: Usa un bucle for...of para recorrer todos los juguetes y elimina los que incluyan la palabra gato (también podéis crear uno nuevo con solo los que NO incluyan esa palabra).
+// Recuerda usar la función .includes() para comprobar la palabra.
+// Puedes usar este array:
+
 const toys = [
     {id: 5, name: 'Transformers'}, 
     {id: 11, name: 'LEGO'}, 
--- ejercicio23.js
+++ ejercicio23.js
@@ -1,3 +1,7 @@
+// Usa un bucle para crear 3 arrays de películas filtrados por categorías.
+// Pelicula pequeña -> menos de 100 minutos, película mediana -> más de 100 minutos y menos de 200 y pelicula grande -> más de 200 minutos.
+// Imprime cada array por consola.
+
 const movies = [
     { name: "Titan A.E.", durationInMinutes: 130 },
     { name: "Nightmare before Christmas", durationInMinutes: 225 },
